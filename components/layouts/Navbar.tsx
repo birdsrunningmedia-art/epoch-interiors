789dbf781dc02abfd75c0f995ecedcfa0c325f9f
Yekini Abdulwahab <birdsrunningmedia@gmail.com>
2025-11-22 13:18:18 +0100

    added interiors

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import { Menu } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="fixed top-12">
+      <Image src={"/images/logo.png"} alt={"logo"} height={61} width={161} />
+      <ul>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Services</li>
+      </ul>
+      <Menu />
+    </nav>
+  );
 }
